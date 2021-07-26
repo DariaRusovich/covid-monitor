43f802d809329e4f22ad68b9ded2623b6920af3e
DariaRusovich <rusovichdasha41@gmail.com>
2021-07-26 22:34:28 +0300

    get-data

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
-fetch('https://api-covid19.rnbo.gov.ua/data?to=2021-07-23')
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+
+async function getData(url) {
+  const response= await fetch(url)
+  const DATA = await response.json()
+  console.log(DATA.ukraine);
+}
+
+getData('https://api-covid19.rnbo.gov.ua/data?to=2021-07-23')
